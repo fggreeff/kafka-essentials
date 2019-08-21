70a0b8b48904f5d190af2a54fd6c76f692c0e614
Francois Greeff <francois.greeff@bglgroup.co.uk>
2019-08-21 17:49:16 +0100

    Add consumer

---
+++ src/main/java/com/github/fggreeff/kafka/tutorial1/ConsumerDemo.java
@@ -0,0 +1,54 @@
+package com.github.fggreeff.kafka.tutorial1;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class ConsumerDemo {
+    public static void main(String[] args) {
+
+        Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
+        String bootstrapServer = "127.0.0.1:9092";
+        String groupId = "my-fourth-app";
+        String topic = "first_topic";
+
+
+        //create consumer properties
+        Properties properties = new Properties();
+
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG,groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest"); // see usage for "latest"
+
+        //create consumer
+        KafkaConsumer<String,String> consumer = new KafkaConsumer<String, String>(properties);
+
+        //subscribe consumer to topic
+        consumer.subscribe(Collections.singleton(topic)); //can use a list of topics: Arrays.asList("t1", "t2")
+
+        // poll for new data (just for testing, not for production )
+        while(true){
+           ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+           for (ConsumerRecord<String, String> record:records ){
+               logger.info("Key: " + record.key() + " value: " + record.value());
+               logger.info("Partition: " + record.partition() + " Offset: " + record.offset());
+
+           }
+
+        }
+    }
+}
