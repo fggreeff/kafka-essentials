96d415c08422f1d4eaa67c36bf2d5e0ede6241c2
Francois Greeff <francois.greeff@bglgroup.co.uk>
2020-01-18 18:49:54 +0000

    Create kafka consumer to add tweets to elastic search

--- kafka-basics/src/main/java/kafka/tutorial1/ConsumerDemoGroups.java
+++ kafka-basics/src/main/java/kafka/tutorial1/ConsumerDemoGroups.java
@@ -15,35 +15,35 @@ import java.util.Properties;
 public class ConsumerDemoGroups {
     public static void main(String[] args) {
 
-        Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
-        String bootstrapServer = "127.0.0.1:9092";
-        String groupId = "my-fifth-app";
-        String topic = "first_topic";
+            Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
+            String bootstrapServer = "127.0.0.1:9092";
+            String groupId = "my-fifth-app";
+            String topic = "first_topic";
 
 
-        //create consumer properties
-        Properties properties = new Properties();
+            //create consumer properties
+            Properties properties = new Properties();
 
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG,groupId);
-        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest"); // see usage for "latest"
+            properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+            properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+            properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+            properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG,groupId);
+            properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest"); // see usage for "latest"
 
-        //create consumer
-        KafkaConsumer<String,String> consumer = new KafkaConsumer<String, String>(properties);
+            //create consumer
+            KafkaConsumer<String,String> consumer = new KafkaConsumer<String, String>(properties);
 
-        //subscribe consumer to topic
-        consumer.subscribe(Collections.singleton(topic)); //can use a list of topics: Arrays.asList("t1", "t2")
+            //subscribe consumer to topic
+            consumer.subscribe(Collections.singleton(topic)); //can use a list of topics: Arrays.asList("t1", "t2")
 
-        // poll for new data (just for testing, not for production )
-        while(true){
-           ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-           for (ConsumerRecord<String, String> record:records ){
-               logger.info("Key: " + record.key() + " value: " + record.value());
-               logger.info("Partition: " + record.partition() + " Offset: " + record.offset());
+            // poll for new data (just for testing, not for production )
+            while(true){
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<String, String> record:records ){
+                    logger.info("Key: " + record.key() + " value: " + record.value());
+                    logger.info("Partition: " + record.partition() + " Offset: " + record.offset());
 
-           }
+                }
 
         }
     }
